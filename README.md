80b9746bb1df58b27ac81b3b3ba7f84f64b868d9
mateo <mateo.tarasiuk@gmail.com>
2020-10-05 08:30:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bible App
+
+Website written in [NodeJS](https://github.com/nodejs/node)
+
+## Live version
+
+https://biblia-api.kosciolwroclaw.pl
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const variables = require('../data/variables.json')
+const data = [
+  require('../data/EIB.json'),
+  require('../data/UBG.json'),
+  require('../data/BW.json'),
+  require('../data/KJV.json'),
+  require('../data/BBE.json'),
+]
+
+const app = express()
+
+app.get('/', (req, res) => res.json(variables))
+app.get('/:bible', ({params}, res) => res.json(data[params.bible]))
+app.get('/:bible/:book', ({params}, res) => res.json(data[params.bible][params.book]))
+app.get('/:bible/:book/:chapter', ({params}, res) => res.json(data[params.bible][params.book][params.chapter]))
+
+app.listen(3000)
