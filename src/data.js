92b71e1c7b46cbe2966e1cb6629b51e03c80ecb0
mateo <mateo.tarasiuk@gmail.com>
2020-11-25 08:57:56 +0100

    Add extra info for API routes

--- src/data.js
+++ src/data.js
@@ -12,35 +12,61 @@ const getVariables = () => {
 }
 
 const getBible = ({bible}) => {
-  const list = []
+  const verses = []
 
   for (const book in data[bible]) {
-    list.push([])
+    verses.push([])
 
     for (const chapter of data[bible][book]) {
-      list[book].push(chapter.length)
+      verses[book].push(chapter.length)
     }
   }
 
-  return list
+  return {
+    bible_ID: +bible,
+    bible: variables.bibles[bible],
+    books: variables.books,
+    verses: verses
+  }
 }
 
 const getBook = ({bible, book}) => {
-  const list = []
+  const verses = []
 
   for (const chapter of data[bible][book]) {
-    list.push(chapter.length)
+    verses.push(chapter.length)
   }
 
-  return list
+  return {
+    bible_ID: +bible,
+    bible: variables.bibles[bible],
+    book_ID: +book,
+    book: variables.books[book],
+    verses: verses
+  }
 }
 
 const getChapter = ({bible, book, chapter}) => {
-  return data[bible][book][chapter]
+  return {
+    bible_ID: +bible,
+    bible: variables.bibles[bible],
+    book_ID: +book,
+    book: variables.books[book],
+    chapter_ID: +chapter,
+    verses: data[bible][book][chapter]
+  }
 }
 
 const getVerse = ({bible, book, chapter, verse}) => {
-  return data[bible][book][chapter][verse]
+  return {
+    bible_ID: +bible,
+    bible: variables.bibles[bible],
+    book_ID: +book,
+    book: variables.books[book],
+    chapter_ID: +chapter,
+    verse_ID: +verse,
+    verse: data[bible][book][chapter][verse]
+  }
 }
 
 module.exports = {
